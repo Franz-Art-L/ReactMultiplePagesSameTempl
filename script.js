92d5ca4bcf6bd3f7431ec091602cb4ea98a27190
Franz-Art-L <francislandia@icloud.com>
2022-03-28 22:53:15 +0800

    done building a page template

--- script.js
+++ script.js
@@ -1,6 +1,78 @@
-var element = React.createElement(
-    "h1", { className: "greeting" },
-    " Hello World! "
-);
+var Navbar = function Navbar() {
+    return React.createElement(
+        "nav",
+        { className: "navbar navbar-expand-lg navbar-light bg-light" },
+        React.createElement(
+            "a",
+            { className: "navbar-brand",
+                href: "#" },
+            " Navbar "
+        ),
+        " "
+    );
+};
 
-ReactDOM.render(element, document.getElementById('root'));
+var Sidebar = function Sidebar() {
+    return React.createElement(
+        "div",
+        { className: "d-none d-md-block col-md-3" },
+        React.createElement(
+            "div",
+            { className: "border border-primary py-4 px-3" },
+            "Sidebar "
+        ),
+        " "
+    );
+};
+
+var Footer = function Footer() {
+    return React.createElement(
+        "div",
+        { className: "border-top p-2" },
+        "Template Demo & copy; 2022. "
+    );
+};
+
+var Template = function Template(props) {
+    return React.createElement(
+        React.Fragment,
+        null,
+        " ",
+        React.createElement(Navbar, null),
+        React.createElement(
+            "div",
+            { className: "container py-4" },
+            React.createElement(
+                "div",
+                { className: "row" },
+                React.createElement(
+                    "div",
+                    { className: "col-12 col-md-9" },
+                    " ",
+                    props.children,
+                    " "
+                ),
+                " ",
+                React.createElement(Sidebar, null)
+            ),
+            " "
+        ),
+        " ",
+        React.createElement(Footer, null)
+    );
+};
+
+var App = function App() {
+    return React.createElement(
+        Template,
+        null,
+        React.createElement(
+            "h1",
+            null,
+            " Main Content "
+        ),
+        " "
+    );
+};
+
+ReactDOM.render(React.createElement(App, null), document.getElementById('root'));
--- src/script.js
+++ src/script.js
@@ -1,3 +1,60 @@
-const element = < h1 className = "greeting" > Hello World! < /h1>;
+ const Navbar = () => {
+     return ( <
+         nav className = "navbar navbar-expand-lg navbar-light bg-light" >
+         <
+         a className = "navbar-brand"
+         href = "#" > Navbar < /a> < /
+         nav >
+     );
+ };
 
-ReactDOM.render(element, document.getElementById('root'));
+ const Sidebar = () => {
+     return ( <
+         div className = "d-none d-md-block col-md-3" >
+         <
+         div className = "border border-primary py-4 px-3" >
+         Sidebar <
+         /div> < /
+         div >
+     );
+ };
+
+ const Footer = () => {
+     return ( <
+         div className = "border-top p-2" >
+         Template Demo & copy; 2022. <
+         /div>
+     );
+ };
+
+ const Template = (props) => {
+     return ( < React.Fragment > <
+         Navbar / >
+         <
+         div className = "container py-4" >
+         <
+         div className = "row" >
+         <
+         div className = "col-12 col-md-9" > { props.children } <
+         /div> <
+         Sidebar / >
+         <
+         /div> < /
+         div > <
+         Footer / >
+
+         <
+         /React.Fragment>
+     );
+ };
+
+ const App = () => {
+     return ( <
+         Template >
+         <
+         h1 > Main Content < /h1> < /
+         Template >
+     )
+ }
+
+ ReactDOM.render( < App / > , document.getElementById('root'));
